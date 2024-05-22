81b1693a9e5887e62f3bc49671fbc6375d65a30d
DrAzrael <u8_aleszl_lbn@technischools.com>
2024-05-22 21:48:12 +0200

    update gannta

--- gantt/main.py
+++ gantt/main.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 # Dane dla wykresu Gantta
-tasks = ['pisanie back \n endu(Wojtek)', 'pisanie frontendu\n(Aleks)', 'integracja frontendu \nz backendem(Aleksy)']
-start_dates = ['2024-04-25', '2024-04-25', '2024-05-16']
-end_dates = ['2024-05-16', '2024-05-16', '2024-05-19']
+tasks = ['design i rozpiska \nfunkcjonalności\n(Aleks)', 'pisanie kodu\n(Wojtek)', 'bug testing/próby zepsucia\n(Aleks)']
+start_dates = ['2024-04-25', '2024-05-03', '2024-05-16']
+end_dates = ['2024-05-03', '2024-05-16', '2024-05-19']
 
 # Konwersja dat na format matplotlib
 start_dates = [mdates.datestr2num(date) for date in start_dates]
